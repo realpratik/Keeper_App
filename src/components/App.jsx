4f88e7e2b740c484f5512df61f8a6eda1e1729a1
Pratik <danpheaus@gmail.com>
2022-02-13 09:57:05 +1000

    Initial Commit

---
+++ src/components/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+
+// function createNotes(noteItem){
+//   return <Note key= {noteItem.key} title ={noteItem.title} content={noteItem.content} />
+// }
+
+function App() {
+  return <div>
+    <Header />
+    {/* <Note  title="This is the title."  content="This is the content." /> */}
+    {notes.map( noteItem =>  (
+      <Note 
+        key= {noteItem.key} 
+        title ={noteItem.title} 
+        content={noteItem.content}
+       /> 
+    ))};
+    <Footer />
+  </div>
+}
+
+export default App;
