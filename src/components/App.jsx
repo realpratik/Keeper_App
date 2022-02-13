8b803a1aa0ea623b1bdfbc4bb409a1f9f0f1af31
Pratik <danpheaus@gmail.com>
2022-02-13 10:13:07 +1000

    Initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -25,3 +25,21 @@ function App() {
 }
 
 export default App;
+
+
+// …or create a new repository on the command line
+// echo "# CSVPHP" >> README.mdgit 
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/realpratik/CSVPHP.git
+// git push -u origin main
+
+
+
+
+// …or push an existing repository from the command line
+// git remote add origin https://github.com/realpratik/CSVPHP.git
+// git branch -M main
+// git push -u origin main
